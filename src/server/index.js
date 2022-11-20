410cb08526147db9b4bb205cfc89f72541868312
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-11-20 19:12:35 +0200

    web socket

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,36 @@
  //TODO listen to requests 
 
 // this will run on node js 
+
+var WebSocketServer = require('ws').Server;
+
+
+
+
+function log(text){
+    var time = new Date();
+    console.log("[" + time.toLocaleTimeString() + "] " + text);
+}
+
+let httpserver = null; 
+
+try{
+
+}catch(err){
+    log(err); 
+}
+
+function handleHTTPRequest(request, response){
+    log("Received requests for: " + request.url); 
+    //TODO handle http requests 
+    response.writeHead(); 
+    response.end(); 
+}
+
+httpserver.listen(6503, function(){
+    log("Server is listening on port 6503"); 
+}); 
+
+let webserver = new WebSocketServer({
+    
+});
