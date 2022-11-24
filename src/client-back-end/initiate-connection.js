51285b91588d11b2f2aaf16b3c6a596f6fdedf14
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-11-24 22:10:55 +0200

    added initialization code to function

--- src/client-back-end/initiate-connection.js
+++ src/client-back-end/initiate-connection.js
@@ -1,4 +1,5 @@
 
+import { media_functions } from "./media-handler.js";
 
 
 function log(text){
@@ -10,10 +11,21 @@ function log(text){
 
 export async function initiateConnection(connection){
     try{
+        let localStream = await media_functions.getMedia({
+            video: true, 
+            audio: true,
+        });
+        document.getElementById('local-video').srcObject = localStream; 
+
+        //add tracks into stream 
+        localStream.getTracks().forEach((track => {
+            connection.addTrack(track, localStream); 
+        }));
+
         let offer = await connection.createOffer(); 
         await connection.setLocalDescription(offer);
         //TODO send the offer to the signalling server 
-        return offer; 
+        return Promise.resolve(offer); 
     }catch(error){
         console.log("error in iniatiate connection", error);
     }
