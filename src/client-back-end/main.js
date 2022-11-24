3cc96742bbab3a742353f54e17be7f263930a6a6
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-11-24 22:10:33 +0200

    eventListeners, moved initiate connection function
    Added on track and on ice candidate event listeners.

--- src/client-back-end/main.js
+++ src/client-back-end/main.js
@@ -1,8 +1,12 @@
 
-import { media_functions } from "./media-handler.js";
 import {initiateConnection} from "./initiate-connection.js"; 
 import {handleOffer, handleTrack} from "./receive-connection.js"
 
+
+function log(text){
+    var time = new Date();
+    console.log("[" + time.toLocaleTimeString() + "] " + text);
+}
 
 const default_configuration = {
     iceServers: [
@@ -22,28 +26,19 @@ let connection = new RTCPeerConnection(default_configuration);
 let remoteStream = new MediaStream(); 
 
 
-let startTheConnection = async() => {
-    let localStream = await media_functions.getMedia({
-        video: true, 
-        audio: true,
-    });
-    document.getElementById('local-video').srcObject = localStream; 
-    offer = await initiateConnection(connection, localStream);
-    return Promise.resolve(offer); 
-};  
+connection.onicecandidate = async (event) => {
+    if(event.candidate) {
+        log("New ICE candidate: " + event.candidate);
+    }
+}
 
-connection.ontrack((track) => {}); 
+connection.ontrack = (event) => {
+    log("tracking event: " + event); 
+    event.streams[0].getTrack().forEach((track) =>{
+        remoteStream.addTrack(track, localStream); 
+    })
+} 
+
+await initiateConnection(connection);
 
 
-startTheConnection()
-.then((offer) => {
-    //TODO obviously it should not be here and it should be called once the
-    //server sends the offer 
-    console.log("Description of the offer: ",offer);
-    handleOffer(connection, offer); 
-})
-.catch((err) => {
-    alert(err); 
-}); 
-
-console.log("Handling offer")
