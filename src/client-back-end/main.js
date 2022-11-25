8f2ac1bbbe4f3052575d6a89f5051907b6c2e69c
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-11-25 19:48:42 +0200

    moved to connection handler

--- src/client-back-end/main.js
+++ src/client-back-end/main.js
@@ -1,42 +1,28 @@
-
-import {initiateConnection} from "./initiate-connection.js"; 
+import {createPeerConnection} from "./connection-handler"; 
 
 function log(text){
     var time = new Date();
     console.log("[" + time.toLocaleTimeString() + "] " + text);
 }
 
-const default_configuration = {
-    iceServers: [
-        {
-            urls:[
-                'stun:stun1.l.google.com:19302',
-                'stun:stun2.l.google.com:19302',
-                'stun:stun3.l.google.com:19302',
-                'stun:stun4.l.google.com:19302'
-            ]
-        }
-    ]
-};
+
 
 let offer = null; 
-let connection = new RTCPeerConnection(default_configuration); 
 let remoteStream = new MediaStream(); 
 
+let clientID = 0; 
+let myUsername = null; 
 
-connection.onicecandidate = async (event) => {
-    if(event.candidate) {
-        log("New ICE candidate: " + event.candidate);
-    }
+
+function createIdentifier(){
+    //TODO create a unique identifier so we can handle connections in the server 
+
+    // ----- TASK  ------- 
 }
 
-connection.ontrack = (event) => {
-    log("tracking event: " + event); 
-    event.streams[0].getTrack().forEach((track) =>{
-        remoteStream.addTrack(track, localStream); 
-    })
-} 
+function connect(){
+    let serverURL; 
 
-await initiateConnection(connection);
 
+}
 
