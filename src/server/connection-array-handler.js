814ee9eb5724ad68b878c596ff172c174ab83f27
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-11 05:39:05 +0200

    Throwing errors
    The get functions inside active connection handler now throw appropriate errors depending on if the connection exists.

--- src/server/connection-array-handler.js
+++ src/server/connection-array-handler.js
@@ -29,7 +29,44 @@ module.exports =  {
         active_connections = active_connections.filter((connection) => connection.connected);
     },  
     
+    /**
+     * Finds the connection with the specific client id.(hopefully unique)
+     * @param {*} clientID the id we want the connection for 
+     * @returns the connection with the given client ID
+     * @throws {*} connection doesn't exist error 
+     */
+    getClientIDConnection: function(clientID){
+        let connection = active_connections.find((connection) => {
+            connection.user_id == clientID
+        });
+
+        if(!connection){
+            throw new Error("connection doesn't exist"); 
+        }
+        return connection; 
+    },
+
+    /**
+     * Finds all connections with the specified room code. 
+     * @param {*} room_code the room code that we want to receive the connections for
+     * @returns the connections (or connection) with the specified room code
+     * @throws {*} connection with the specified room code does not exist error
+     */
+    getRoomCodeConnections: function(room_code){
+        let connections = active_connections.filter((connection) => connection.room_code == room_code);
+
+        if(!connections){
+            throw new Error("connection with the specified room code does not exist");
+        }
+
+        return connections;  
+    },
     
+    /**
+     * Creates a unique identifier for the user based on the sha-256 algorithm. 
+     * @param {*} identifier a sequence number increased by one with each connection
+     * @returns the hash hex produced. 
+     */
     createIdentifierForUser: async function(identifier){
         const key = "newuserjoins"
         // encode as UTF-8
