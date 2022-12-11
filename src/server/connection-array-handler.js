479e6ea6e360f4f685da5e5e49bb81b10a795a1c
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-11 02:44:04 +0200

    Added functions for connection handling
    AddUserToRoom: Adds user to the specific room if it exists.
    CreateRoom: creates new room
    doesRoomExist: finds the OBJECT room if it exists

--- src/server/connection-array-handler.js
+++ src/server/connection-array-handler.js
@@ -1,7 +1,7 @@
 
 
-let connection_array = []; 
-
+let active_connections = []; 
+let rooms = []; 
 
 function log(text){
     var time = new Date();
@@ -10,14 +10,61 @@ function log(text){
 
 module.exports =  {
 
+
+    createRoom: function(room_code){
+        log("Creating new room"); 
+        //users are added on the connection based on the room code 
+        rooms.push({
+            code: room_code,
+            users: [], 
+        }); 
+    },
+
+    /**
+     * Adds user to the specific room if it exists. 
+     * @param {*} room the room OBJECT that the user is going to be added to 
+     * @param {*} clientID the user that is going to be added to the room 
+     * @returns 
+     */
+    addUserToRoom: function(room, clientID){
+        if(!room){
+            log("given erroneous room"); 
+            return;  
+        }
+        room.users.push(clientID);
+    },
+
+     /**
+     * Finds the OBJECT room with the specific room code. 
+     * @param {*} room_code the room code that we'll check if it exists 
+     */
+    doesRoomExists: function(room_code){
+        let room = rooms.find((room) => room.code == room_code);
+
+        if(!room){
+            log("room doesn't exist"); 
+            return 
+        }
+        return room; 
+    },
+
+    /**
+     * Adds new connection to the active connections array 
+     * @param {*} connection new web socket connection
+     */
     addConnection: function (connection){
         log("Adding new connection to array"); 
-        connection_array.push(connection);
+        active_connections.push(connection);
     },
 
+    /**
+     * filter all non dead connections using connection.connected 
+     */
     removeConnection: function(){
-
-    },
+        log("Removing connection from array");
+        active_connections = active_connections.filter((connection) => connection.connected);
+    },  
+    
     
     createIdentifierForUser: async function(identifier){
         const key = "newuserjoins"
