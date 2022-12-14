56d6c855a5ef0c7741a5910f9ac14e8dc7c2928e
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-12-14 08:01:46 +0200

    Warning: potential bad decision
    When broadcasting to room participants we must not send the message back to the source. Added forced id in message in order to avoid that

--- src/server/send-data.js
+++ src/server/send-data.js
@@ -15,18 +15,26 @@ module.exports = {
      * @throws {*} any error caught by the underlying active connection handler functions 
      */
     sendToOneUser: function(connection, message){
-        log("Sending data to specific user");
+        log("Sending data: " +  message.type + " to specific user");
         connection.send(JSON.stringify(message)); 
     },
 
 
     /**
-     * "Broadcasts" a message to all the specified room participants. 
+     * "Broadcasts" a message to all the specified room participants. Message must have an ID specified. 
+     * The ID here essentially acts the same as a source and prevents the messages to be broadcasted 
+     * back to the sender. 
      * @param {*} room_code the room that the message is going to broacasted 
-     * @param {*} message the message that is going to be broadcasted. 
+     * @param {*} message the message that is going to be broadcasted. Must have a client id specified to determine origin. 
      * @throws {*} any error caught by the underlying active connection handler functions 
      */
     sendToRoomParticipants: function(room_code, message){
+
+
+        if(!("id" in message) || !message.id){
+            throw new Error("message doesn't have an ID specified");
+        }
+
         let current_room = null , current_room_connections = null; 
         try{
             ({current_room,current_room_connections}= room_handlers.getConnectionsFromRoom(room_code));
@@ -38,10 +46,14 @@ module.exports = {
             throw new Error("no connection to send the data to"); 
         }
 
-        log("Sending data to room participants");
+        log("Sending data:" + message.type + " to room participants");
 
         current_room_connections.forEach((connection) => {
-            connection.send(JSON.stringify(message));
+            //don't broadcast to yourself. 
+            if(connection.user_id !== message.id){
+                connection.send(JSON.stringify(message));
+            }
+            
         });
     }, 
 
